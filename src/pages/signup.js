39c00c8131c7dead438aaf39c2a6951267c629e2
Jezh Technologies <114339951+jezh-technologies@users.noreply.github.com>
2023-10-31 16:32:25 +0530

    Add files via upload

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -46,7 +46,7 @@ function Sign() {
 
   return (
    
-    <html className="bg-black">
+    <html>
     <Head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
@@ -54,80 +54,182 @@ function Sign() {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"/>
 
 </Head>
-<body className="bg-black d-none d-lg-block">    
-<nav className="navbar bg-black border-bottom">
-<div className="container-fluid justify-content-center px-0">
-  <h1 className="navbar-brand my-0 h1 fw-bolder text-white"><b>Dakshin Sahodaya</b></h1>      
-  <span className="badge text-bg-light text-black mt-1">School Portal</span>
-</div>
-</nav>
+<body className="d-none d-lg-block"> 
+<header>
+        <div px-5>
+          <nav class="navbar navbar-expand-lg bg-body-tertiary px-lg-5 px-0 border border-bottom ">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/">
+              <div class="d-flex flex-row align-items-center">
+                <div class="p-2">
+                  <img src="/images/dakshilogo.jpg" alt="Dakshin Sahodaya" width="75" height="60" className="d-inline-block img-fluid rounded-1 align-text-top"/>
+                </div>
+                <div className="p-2">
+                <h2 className="fw-bolder">Dakshin<br/>Sahodaya</h2>
+                </div>
+              </div>
+              </a>
+              <button class="navbar-toggler border-1  rounded-circle p-3 shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav fw-bolder h4 mx-auto">
+                  <a class="nav-link p-3 mx-lg-2 mx-0 active" aria-current="page" href="/">Home</a>
+                  <a class="nav-link p-3 mx-lg-2 mx-0" href="#">Registered Schools</a>
+                  <a class="nav-link p-3 mx-lg-2 mx-0" href="/gallery">Gallery</a>
+                  <a class="nav-link p-3 mx-lg-2 mx-0"></a>
+                </div>
+                <button type="button" className="btn btn-success btn-lg border-0 rounded-4 fw-bold px-5 py-3 me-3 shadow-sm" style={{backgroundImage:`linear-gradient(90deg,#1D976C,#93F9B9)`}} onClick={()=>{Router.push("/signup")}} >Register now</button>
 
-<div className="container-fluid justify-content-center bg-black py-5" style={{width:'60em'}}>
+              </div>
+            </div>
+          </nav>
+        </div>
+
+
+</header>
+
+
+
+<div className="container justify-content-center py-5" >
+<p className="display-5 fw-bolder pb-1" align="center">Registration Form</p>
+
 <form class="row g-3">
-  <div class="col-md-4">
+<div class="col-md-9">
   <div class="form-floating">
-  <input type="text" class="form-control bg-dark text-white border-0" placeholder="Password" required  onChange={(e)=>{setprincipal(e.target.value)}} value={principal} />
-  <label class="text-secondary fw-bold">Principal name</label>
-</div>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
-  </div>
-  <div class="col-md-4">
-  <div class="form-floating">
-  <input type="email" class="form-control bg-dark text-white border-0" placeholder="Password" required value={email} onChange={(e)=>{setemail(e.target.value)}}/>
-  <label class="text-secondary fw-bold">E-mail address</label>
-</div>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
-  </div>
-  <div class="col-md-4">
-  <div class="form-floating">
-  <input type="tel" class="form-control bg-dark text-white border-0" placeholder="Password" value={contact} onChange={(e)=>{setcontact(e.target.value)}}/>
-  <label class="text-secondary fw-bold">Contact number</label>
-</div>
-  </div>
-  <div class="col-md-9">
-  <div class="form-floating">
-  <input type="text" class="form-control bg-dark text-white border-0" placeholder="Password" value={school} onChange={(e)=>{setschool(e.target.value)}}/>
-  <label class="text-secondary fw-bold">School name</label>
+  <input type="text" class="form-control text-dark " placeholder="Password" value={school} onChange={(e)=>{setschool(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">School Name</label>
 </div>
   </div>
   <div class="col-md-3">
   <div class="form-floating">
-  <input type="" class="form-control bg-dark text-white border-0" placeholder="Password" value={code} onChange={(e)=>{setcode(e.target.value)}}/>
-  <label class="text-secondary fw-bold">Affiliation Code</label>
+  <input class="form-control text-dark" type="number" required placeholder='Affiliation Code' value={code} onChange={(e)=>{setcode(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">Affiliation Code</label>
 </div>
   </div>
-  
-  <div class="col-md-12">
+  <div className="col-md-12">
   <div class="form-floating">
-  <textarea type="text" class="form-control bg-dark text-white border-0" rows={10} cols={5} placeholder="Password" value={address} onChange={(e)=>{setaddress(e.target.value)}}/>
-  <label class="text-secondary fw-bold">Address</label>
+  <textarea class="form-control text-dark" required type="text" placeholder="Address" value={address} onChange={(e)=>{setaddress(e.target.value)}} style={{height: "100px"}}></textarea>
+  <label class="text-secondary fw-semibold">Address</label>
+  </div>
+  </div>
+
+
+
+
+
+  <div class="col-md-3">
+  <div class="form-floating">
+  <input type="text" class="form-control text-dark" placeholder="City" required  onChange={(e)=>{setprincipal(e.target.value)}} value={principal} />
+  <label class="text-secondary fw-semibold">City</label>
 </div>
-<div class="col-md-12">
-<div class="form-floating">
-  <textarea class="form-control bg-dark text-white border-0" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-  <label for="floatingTextarea2">Comments</label>
+
+  </div>
+  <div class="col-md-3">
+  <div class="form-floating">
+  <input type="number" class="form-control text-dark" placeholder="Pin Code" required value={email} onChange={(e)=>{setemail(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">Pin Code</label>
 </div>
+
+  </div>
+  <div class="col-md-3">
+  <div class="form-floating">
+  <input type="text" class="form-control text-dark" required placeholder="District" value={contact} onChange={(e)=>{setcontact(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">District</label>
 </div>
   </div>
+  <div class="col-md-3">
+  <div class="form-floating">
+  <input type="text" class="form-control text-dark" placeholder="State" value={contact} onChange={(e)=>{setcontact(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">State</label>
+</div>
+  </div>
+
+
+  <div class="col-md-4">
+  <div class="form-floating">
+  <input type="tel" class="form-control text-dark" placeholder="Telephone Number" required value={email} onChange={(e)=>{setemail(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">Telephone Number</label>
+</div>
+    <div class="valid-feedback">
+      Looks good!
+    </div>
+  </div>
+  <div class="col-md-4">
+  <div class="form-floating">
+  <input type="email" class="form-control text-dark" placeholder="Email ID" value={contact} onChange={(e)=>{setcontact(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">Email ID</label>
+</div>
+  </div>
+  <div class="col-md-4">
+  <div class="form-floating">
+  <input type="tel" class="form-control text-dark" placeholder="Password" value={contact} onChange={(e)=>{setcontact(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">Website</label>
+</div>
+  </div>
+
+  
   <div class="col-md-6">
   <div class="">
-  <label for="formFile" class="form-label text-white fw-bold">Upload the School's logo</label>
-  <input class="form-control bg-dark border-0 text-secondary fw-bold" type="file" id="formFile" onChange={(e)=>{setlogo(e.target.files[0])}}/>
+  <label for="formFile" class="form-label text-dark fw-bold px-1">Upload the School's logo</label>
+
+  <input class="form-control text-dark fw-semibold" type="file" id="formFile" onChange={(e)=>{setlogo(e.target.files[0])}}/>
+  <p className="fw-semibold text-small mt-2">
+    <span className='text-danger fw-bolder '>*</span>The logo should have a white background<br/>
+    <span className='text-danger fw-bolder '>*</span>The logo should be of the size 500x500</p>
 </div>
 </div>
+
 <div class="col-md-6">
 <div class="">
-  <label for="formFile" class="form-label text-white fw-bold">Upload the Payment photo</label>
-  <input class="form-control bg-dark border-0 text-secondary fw-bold" type="file" id="formFile"  onChange={(e)=>{setpayment(e.target.files[0])}}/>
+  <label for="formFile" class="form-label text-dark fw-bold px-1">Upload the Payment photo</label>
+  <input class="form-control text-dark fw-semibold" type="file" id="formFile"  onChange={(e)=>{setpayment(e.target.files[0])}}/>
 </div>
 </div>
-<div class="d-grid mt-3">
-  <button class="btn btn-lg text-black fw-bold bg-warning border-0 py-2" type="button" onClick={sign}>submit</button>
+<hr/>
+
+  <div class="col-md-4">
+  <div class="form-floating">
+  <input type="text" class="form-control text-dark" placeholder="Principl Name" required value={email} onChange={(e)=>{setemail(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">Principl Name</label>
 </div>
+
+  </div>
+  <div class="col-md-4">
+  <div class="form-floating">
+  <input type="tel" class="form-control text-dark" required placeholder="WhatsApp Number" value={contact} onChange={(e)=>{setcontact(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">WhatsApp Number</label>
+</div>
+  </div>
+  <div class="col-md-4">
+  <div class="form-floating">
+  <input type="email" class="form-control text-dark" placeholder="Principal Email ID" required value={contact} onChange={(e)=>{setcontact(e.target.value)}}/>
+  <label class="text-secondary fw-semibold">Principal Email ID</label>
+</div>
+  </div>
+<div class="d-grid py-3">
+  <button type="button" className="btn btn-success btn-lg border-0 rounded-3 fw-bold px-5 py-3 shadow-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop" style={{backgroundImage:`linear-gradient(90deg,#1D976C,#93F9B9)`}} onClick={sign}>submit</button>
+
+
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header border border-0">
+        <button type="button" class="btn-close border rounded-circle" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body text-center">
+      <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" fill="currentColor" class="bi bi-check2-circle text-success" viewBox="0 0 16 16">
+  <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+  <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+</svg>
+<h1 class="display-5 fw-semibold">Successfully Registered !</h1>
+
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
 </form>
 </div>
 
@@ -138,21 +240,17 @@ function Sign() {
 
 
 
+<footer className="navbar navbar-expand-lg bg-light px-lg-5 px-2 text-dark border border-top-1">
+    <div className="container-fluid">
+      <span className="navbar-text me-auto  px-2">
+        <span className="fw-bold">Copyright © 2023 DSSC </span><div class="vr text-black mx-2"></div>
+        <small className="fw-normal"><span className="fw-bold">Designed & Developed by</span> <a target="new" href="https://jezhtechnologies.com" className="fw-bolder text-decoration-none text-dark">Jezh Technologies Pvt Ltd.</a></small>
 
-<div className="container-fluid bg-black border-top border-secondary ">
-  <footer className="d-flex flex-wrap justify-content-between align-items-center bg-black py-2">
-    <p className="col-md-4 mb-0 fw-bold text-secondary">© 2022 Dakshin Sahodaya</p>
+      </span>
 
-    <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-    <img src="/images/dakshilogo.jpg" className="img-fluid rounded-1" width="50" height="50" alt="..."/>
-    </a>
-    
-      <ul className="nav col-md-4 justify-content-end">
-        <li className="nav-item"><a href="https://jeshtechnology.com/" className="nav-link px-2 fw-semibold text-secondary">Designed And Developed By
-<b> Jesh Technology Pvt. Ltd.</b></a></li>
-      </ul>
-    </footer>
-</div>  
+    </div>
+  </footer>  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossOrigin="anonymous"></script>
 
 </body>
 </html>
